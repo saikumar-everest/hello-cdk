54fde2d3c32207219f51eeb56eb740849efc2010
Saikumar Vantaku <vsaikumar@woundtech.net>
2020-07-27 08:02:15 +0530

    Add a lambda and associated api-gateway for get, post and delete objects in s3 bucket

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from '@aws-cdk/core';
+import * as widgetService from './widget-service';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new widgetService.WidgetService(this, 'Widgets');
   }
 }
